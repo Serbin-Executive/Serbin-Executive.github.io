b16168084b3b1b0a169e575f9264de1936576edc
aKrivonogova <krivonogova.aline@yandex.ru>
2024-06-30 11:37:29 +0300

    added home link and added blocksset container

--- src/components/Block/index.tsx
+++ src/components/Block/index.tsx
@@ -5,7 +5,8 @@ export const enum blockTypes {
     TEXT = "text",
     PALLETTE = "pallette",
     MIXED = "mixed",
-    OUTLINED = 'outlined'
+    OUTLINED = 'outlined',
+    ROUNDED = "rounded",
 }
 
 export interface IBlockProps {
@@ -13,22 +14,29 @@ export interface IBlockProps {
     width?: string;
     height?: string;
     type?: blockTypes;
+    isActive?: boolean | undefined;
+    additionalSelectors?: string;
     onClick?: () => void
 }
 
 const Block = ({
     children,
+    isActive,
     width = "100%",
     height = "100%",
     type = blockTypes.MIXED,
+    additionalSelectors,
     onClick = () => {return;}
 }: IBlockProps): ReactElement => {
     const blockStyle = {
         width: width,
         height: height,
     };
+
+    const classes = `block ${type} ${additionalSelectors} ${isActive && "active"}`;
+
     return (
-        <div className={`block ${type}`} style={blockStyle} onClick={onClick}>
+        <div className={classes} style={blockStyle} onClick={onClick}>
             {children}
         </div>
     );
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -4,33 +4,38 @@ import BlocksSet from "../../components/BlocksSet";
 import Block from "../../components/Block";
 import { setVariants } from "../../components/BlocksSet";
 import { blockTypes } from "../../components/Block";
+import { Link } from "react-router-dom";
 import './style.css'
 const LandingPage = (): ReactElement => {
     return (
         <DefaultLayout>
-            {/* <h1>Landing page</h1> */}
-            <div className="blocks-set-container">
-                <BlocksSet variant={setVariants.FLEX_ROW}>
-                    <>
-                        <Block type={blockTypes.OUTLINED} width="30%" height="150px">
+            <>
+                {/* <h1>Landing page</h1> */}
+                <div className="blocks-set-container">
+                    <BlocksSet variant={setVariants.FLEX_ROW}>
+                        <>
+                            <Block type={blockTypes.OUTLINED} width="30%" height="150px">
 
-                        </Block>
-                        <Block type={blockTypes.OUTLINED} width="30%" height="150px">
+                            </Block>
+                            <Block type={blockTypes.OUTLINED} width="30%" height="150px">
 
-                        </Block>
-                        <Block type={blockTypes.OUTLINED} width="30%" height="150px">
+                            </Block>
+                            <Block type={blockTypes.OUTLINED} width="30%" height="150px">
 
-                        </Block>
-                        <Block type={blockTypes.OUTLINED} width="30%" height="150px">
+                            </Block>
+                            <Block type={blockTypes.OUTLINED} width="30%" height="150px">
 
-                        </Block>
-                        <Block type={blockTypes.OUTLINED} width="30%" height="150px">
+                            </Block>
+                            <Block type={blockTypes.OUTLINED} width="30%" height="150px">
 
-                        </Block>
+                            </Block>
+                        </>
+                    </BlocksSet>
+                </div>
 
-                    </>
-                </BlocksSet>
-            </div>
+                <h2 className="link-title"><Link to='/courses' className="auth-link large">HOME</Link></h2>
+            </>
+
         </DefaultLayout>
     );
 };
